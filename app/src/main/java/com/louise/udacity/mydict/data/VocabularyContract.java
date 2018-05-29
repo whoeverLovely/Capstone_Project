4154165d117c34e6b1d31ecc29f7b4680f21f35f
yan <leeyan0706@hotmail.com>
2018-05-30 04:36:25 +0800

    Add database setup

--- app/src/main/java/com/louise/udacity/mydict/data/VocabularyContract.java
+++ app/src/main/java/com/louise/udacity/mydict/data/VocabularyContract.java
@@ -1,4 +1,46 @@
 package com.louise.udacity.mydict.data;
 
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class VocabularyContract {
+    // The authority, which is how your code knows which Content Provider to access
+    public static final String AUTHORITY = "com.louise.udacity.mydict";
+
+    // The base content URI = "content://" + <authority>
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    // Define the possible paths for accessing data in this contract
+    public static final String PATH_VOCABULARY = "vocab";
+    public static final String PATH_GROUP = "group";
+
+    public static final long INVALID_VOCABULARY_ID = -1;
+    public static final long INVALID_GROUP_ID = -1;
+
+    public static final class VocabularyEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_VOCABULARY).build();
+
+        public static final String TABLE_NAME = "vocabulary";
+        public static final String COLUMN_WORD = "word";
+        public static final String COLUMN_PHONETIC = "phonetic";
+        public static final String COLUMN_DEFINITION = "definition";
+        public static final String COLUMN_TRANSLATION = "translation";
+        public static final String COLUMN_TAG = "tag";
+        public static final String COLUMN_STATUS = "status";
+        public static final String COLUMN_GROUP_ID = "groupId";
+
+    }
+
+    public static final class GroupEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_GROUP).build();
+
+        public static final String TABLE_NAME = "group";
+        // Define the first vocabulary as the key and mark all vocabularies which linked to it with the same group id
+        public static final String COLUMN_key = "key";
+
+    }
 }
---
+++ app/src/main/java/com/louise/udacity/mydict/data/VocabularyDBHelper.java
@@ -0,0 +1,37 @@
+package com.louise.udacity.mydict.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class VocabularyDBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "my_dict.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public VocabularyDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        final String SQL_CREATE_MOVIES_TABLE = "CREATE TABLE " + VocabularyContract.VocabularyEntry.TABLE_NAME + " (" +
+                VocabularyContract.VocabularyEntry._ID + " INTEGER PRIMARY KEY," +
+                VocabularyContract.VocabularyEntry.COLUMN_WORD + " TEXT NOT NULL, " +
+                VocabularyContract.VocabularyEntry.COLUMN_PHONETIC + " TEXT NOT NULL, " +
+                VocabularyContract.VocabularyEntry.COLUMN_DEFINITION + " TEXT NOT NULL, " +
+                VocabularyContract.VocabularyEntry.COLUMN_TRANSLATION + " TEXT NOT NULL, " +
+                VocabularyContract.VocabularyEntry.COLUMN_TAG + " TEXT NOT NULL, " +
+                VocabularyContract.VocabularyEntry.COLUMN_STATUS + " INTEGER NOT NULL, " +
+                VocabularyContract.VocabularyEntry.COLUMN_GROUP_ID + " INTEGER NOT NULL)";
+
+        db.execSQL(SQL_CREATE_MOVIES_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + VocabularyContract.VocabularyEntry.TABLE_NAME);
+        onCreate(db);
+    }
+}

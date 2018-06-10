0e91ef15309453ba2501da978707b92e81ad30e9
yan <leeyan0706@hotmail.com>
2018-06-11 05:07:08 +0800

    Add settings summary

--- app/src/main/java/com/louise/udacity/mydict/VocabularySettingFragment.java
+++ app/src/main/java/com/louise/udacity/mydict/VocabularySettingFragment.java
@@ -7,9 +7,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v14.preference.MultiSelectListPreference;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
@@ -22,15 +24,18 @@ import java.util.Set;
 import timber.log.Timber;
 
 public class VocabularySettingFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
+        implements Preference.OnPreferenceChangeListener{
 
     Set<String> existingTags;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings);
-        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver,
-                new IntentFilter(VocabularyIntentService.ACTION_STATUS));
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(VocabularyIntentService.ACTION_DOWNLOAD);
+        intentFilter.addAction(VocabularyIntentService.ACTION_DELETE);
+        LocalBroadcastManager.getInstance(getActivity()).registerReceiver(mMessageReceiver, intentFilter);
 
         Preference preference = findPreference(getString(R.string.pref_list_tag_key));
         preference.setOnPreferenceChangeListener(this);
@@ -40,74 +45,78 @@ public class VocabularySettingFragment extends PreferenceFragmentCompat
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-
-
-            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
-
-            String statusType = intent.getStringExtra(VocabularyIntentService.EXTRA_STATUS_TYPE);
+            String action = intent.getAction();
             String status = intent.getStringExtra(VocabularyIntentService.EXTRA_STATUS);
+            switch (action) {
+                case VocabularyIntentService.ACTION_DOWNLOAD:
+                    if (Constants.STATUS_SUCCEEDED.equals(status)) {
+                        Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                R.string.dialog_download_complete,
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                    break;
+                case VocabularyIntentService.ACTION_DELETE:
+                    if (Constants.STATUS_SUCCEEDED.equals(status)) {
+                        Snackbar.make(getActivity().findViewById(android.R.id.content),
+                                R.string.dialog_deletion_completed,
+                                Snackbar.LENGTH_LONG).show();
+                    }
+                    break;
 
-            if (Constants.STATUS_TYPE_DOWNLOAD.equals(statusType)) {
-                if (Constants.STATUS_SUCCEEDED.equals(status)) {
-                    dialogBuilder.setMessage(R.string.dialog_download_complete)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .show();
-                }
-            } else if (Constants.STATUS_TYPE_DELETE.equals(statusType)) {
-                if (Constants.STATUS_SUCCEEDED.equals(status)) {
-                    dialogBuilder.setMessage(R.string.dialog_deletion_completed)
-                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .show();
-                }
+                default:
+                    throw new IllegalArgumentException("The intent action is invalid!");
             }
         }
     };
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-
-
-    }
-
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         Timber.d(preference.getKey() + " updated");
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-        existingTags = sharedPreferences.getStringSet(getString(R.string.pref_list_tag_key), null);
 
-        String prefListTagKey = getString(R.string.pref_list_tag_key);
-        MultiSelectListPreference listPreference = (MultiSelectListPreference)preference;
+        // Set summary for list preference
+        if (preference instanceof ListPreference) {
+            ListPreference listPreference = (ListPreference) preference;
+            int prefIndex = listPreference.findIndexOfValue((String) newValue);
+            String entry = (String) listPreference.getEntries()[prefIndex];
+            Timber.d("the daily count entry is " + entry);
+            listPreference.setSummary(entry);
+        }
 
-        if (prefListTagKey.equals(preference.getKey())) {
-            Set<String> tags = (Set<String>) newValue;
-
-            // Download lists which is not in the existingTags
-            for (String tag : tags) {
-                int prefIndex = listPreference.findIndexOfValue(tag);
-
-                Timber.d("new tag: " + tag);
-                if (existingTags == null ||existingTags.size() == 0 || !existingTags.contains(tag)) {
-
-                    Timber.d("Starting download " + tag);
-                    VocabularyIntentService.startActionDownloadVocabulary(getContext(), tag);
-                }
+        // Set summary for multiselect list preference
+        if (preference instanceof MultiSelectListPreference) {
+            MultiSelectListPreference multiSelectListPreference = (MultiSelectListPreference) preference;
+            Set<String> tagsValue = (Set<String>) newValue;
+            StringBuilder summary = new StringBuilder();
+            for (String s : tagsValue) {
+                int index = multiSelectListPreference.findIndexOfValue(s);
+                String entry = (String) multiSelectListPreference.getEntries()[index];
+                summary = summary.append(entry).append("; ");
             }
+            multiSelectListPreference.setSummary(summary);
 
-            // Deleted lists which is in the exsitingTags but not in tags
-            if (existingTags != null) {
-                for (String tag : existingTags) {
-                    if (tags.size() == 0 || !tags.contains(tag))
-                        VocabularyIntentService.startActionDeleteList(getContext(), tag);
+            // Download and delete related list
+            if (getString(R.string.pref_list_tag_key).equals(preference.getKey())) {
+                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+                existingTags = sharedPreferences.getStringSet(getString(R.string.pref_list_tag_key), null);
+
+                // Download lists which is not in the existingTags
+                for (String tag : tagsValue) {
+                    int prefIndex = multiSelectListPreference.findIndexOfValue(tag);
+
+                    Timber.d("new tag: " + tag);
+                    if (existingTags == null || existingTags.size() == 0 || !existingTags.contains(tag)) {
+
+                        Timber.d("Starting download " + tag);
+                        VocabularyIntentService.startActionDownloadVocabulary(getContext(), tag);
+                    }
+                }
+
+                // Deleted lists which is in the exsitingTags but not in tags
+                if (existingTags != null) {
+                    for (String tag : existingTags) {
+                        if (tagsValue.size() == 0 || !tagsValue.contains(tag))
+                            VocabularyIntentService.startActionDeleteList(getContext(), tag);
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/louise/udacity/mydict/service/VocabularyIntentService.java
@@ -0,0 +1,377 @@
+package com.louise.udacity.mydict.service;
+
+import android.app.IntentService;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.louise.udacity.lib.VocabularyProtos;
+import com.louise.udacity.lib.VocabularyReader;
+import com.louise.udacity.mydict.R;
+import com.louise.udacity.mydict.data.ClientVocabulary;
+import com.louise.udacity.mydict.data.Constants;
+import com.louise.udacity.mydict.data.VocabularyContentProvider;
+import com.louise.udacity.mydict.data.VocabularyContract;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.List;
+
+import timber.log.Timber;
+
+public class VocabularyIntentService extends IntentService{
+
+    public static final String ACTION_DOWNLOAD = "com.louise.udacity.mydict.action.download";
+    public static final String ACTION_STATUS = "com.louise.udacity.mydict.action.status";
+    public static final String ACTION_DELETE = "com.louise.udacity.mydict.action.delete";
+    public static final String ACTION_GENERATE_LEARN_LIST = "com.louise.udacity.mydict.action.list";
+    public static final String ACTION_UPDATE_STATUS = "com.louise.udacity.mydict.action.update-status";
+    public static final String ACTION_UPDATE_REVIEW_LIST = "com.louise.udacity.mydict.action.update-review-list";
+    public static final String ACTION_LINK = "com.louise.udacity.mydict.action.link";
+    public static final String ACTION_SEARCH = "com.louise.udacity.mydict.action.search";
+
+
+    private static final String EXTRA_TAG = "com.louise.udacity.mydict.extra.tag";
+    public static final String EXTRA_STATUS = "com.louise.udacity.mydict.extra.delete-status";
+    public static final String EXTRA_VOCABULARY_ID = "com.louise.udacity.mydict.extra.vocab-id";
+    public static final String EXTRA_VOCABULARY = "com.louise.udacity.mydict.extra.vocabulary";
+    public static final String EXTRA_QUERY = "com.louise.udacity.mydict.extra.query";
+    public static final String EXTRA_GROUP = "com.louise.udacity.mydict.extra.group";
+    public static final String EXTRA_ORIGINAL_VOCABULARY_ID = "com.louise.udacity.mydict.extra.original-vocabulary-id";
+    public static final String EXTRA_ORIGINAL_VOCABULARY_GROUP = "com.louise.udacity.mydict.extra.original-vocabulary-group";
+
+    public VocabularyIntentService() {
+        super("VocabularyIntentService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionDownloadVocabulary(Context context, String tag) {
+        Intent intent = new Intent(context, VocabularyIntentService.class);
+        intent.setAction(ACTION_DOWNLOAD);
+        intent.putExtra(EXTRA_TAG, tag);
+        context.startService(intent);
+    }
+
+    public static void startActionDeleteList(Context context, String tag) {
+        Intent intent = new Intent(context, VocabularyIntentService.class);
+        intent.setAction(ACTION_DELETE);
+        intent.putExtra(EXTRA_TAG, tag);
+        context.startService(intent);
+    }
+
+    public static void startActionUpdateStatus(Context context, int status, long id) {
+        Intent intent = new Intent(context, VocabularyIntentService.class);
+        intent.setAction(ACTION_UPDATE_STATUS);
+        intent.putExtra(EXTRA_TAG, status);
+        intent.putExtra(EXTRA_VOCABULARY_ID, id);
+        context.startService(intent);
+    }
+
+    public static void startActionSearch(Context context, String query) {
+        Intent intent = new Intent(context, VocabularyIntentService.class);
+        intent.setAction(ACTION_SEARCH);
+        intent.putExtra(EXTRA_QUERY, query);
+        context.startService(intent);
+    }
+
+    public static void startActionLink(Context context, ClientVocabulary currentVocabulary, long originalVocabularyId, String originalGroup) {
+        Intent intent = new Intent(context, VocabularyIntentService.class);
+        intent.setAction(ACTION_LINK);
+        intent.putExtra(EXTRA_VOCABULARY, currentVocabulary);
+        intent.putExtra(EXTRA_VOCABULARY_ID, originalVocabularyId);
+        intent.putExtra(EXTRA_GROUP, originalGroup);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (action != null) {
+                switch (action) {
+                    case ACTION_DOWNLOAD:
+                        final String downloadTag = intent.getStringExtra(EXTRA_TAG);
+                        handleActionDownload(downloadTag);
+                        break;
+
+                    case ACTION_DELETE:
+                        final String deleteTag = intent.getStringExtra(EXTRA_TAG);
+                        handleActionDelete(deleteTag);
+                        break;
+
+                    case ACTION_UPDATE_STATUS:
+                        int status = intent.getIntExtra(EXTRA_STATUS, -1);
+                        long vocabId = intent.getLongExtra(EXTRA_VOCABULARY_ID, -1);
+                        handleActionUpdateStatus(status, vocabId);
+                        break;
+
+                    case ACTION_SEARCH:
+                        String query = intent.getStringExtra(EXTRA_QUERY);
+                        handleActionSearch(query);
+                        break;
+
+                    case ACTION_LINK:
+                        ClientVocabulary clientVocabulary = intent.getParcelableExtra(EXTRA_VOCABULARY);
+                        long originalId = intent.getLongExtra(EXTRA_VOCABULARY_ID, -1);
+                        String originalGroup = intent.getStringExtra(EXTRA_GROUP);
+                        handleActionLink(clientVocabulary, originalId, originalGroup);
+                        break;
+
+                    default:
+                        throw new RuntimeException("The action requested:" + action + "is invalid!");
+                }
+            }
+        }
+    }
+
+    private void handleActionLink(ClientVocabulary currentVocabulary, long originalId, String originalGroup) {
+
+        Timber.d("group name received in intentservice is " + originalGroup);
+
+        // Check if current vocabulary exists in db
+        Cursor cursor = getContentResolver().query(VocabularyContentProvider.buildVocabularyUriWithWord(currentVocabulary.getWord()),
+                new String[]{VocabularyContract.VocabularyEntry._ID, VocabularyContract.VocabularyEntry.COLUMN_GROUP_NAME},
+                null,
+                null,
+                null);
+
+        ContentValues cvUpdate = new ContentValues();
+        cvUpdate.put(VocabularyContract.VocabularyEntry.COLUMN_GROUP_NAME, originalGroup);
+
+        // Update original vocabulary
+        int numUpdatedExisting = getContentResolver().update(VocabularyContract.VocabularyEntry.CONTENT_URI,
+                cvUpdate,
+                VocabularyContract.VocabularyEntry._ID + "=?",
+                new String[]{String.valueOf(originalId)});
+        int numUpdatedCurrent = 0;
+
+        // Current vocabulary doesn't exist in db
+        if (cursor.getCount() < 1) {
+            // Insert currentVocabulary to db
+            ContentValues cvInsert = new ContentValues();
+            cvInsert.put(VocabularyContract.VocabularyEntry.COLUMN_WORD, currentVocabulary.getWord());
+            cvInsert.put(VocabularyContract.VocabularyEntry.COLUMN_PHONETIC, currentVocabulary.getPhonetic());
+            cvInsert.put(VocabularyContract.VocabularyEntry.COLUMN_TRANSLATION, currentVocabulary.getTranslation());
+            cvInsert.put(VocabularyContract.VocabularyEntry.COLUMN_DEFINITION, currentVocabulary.getDefinition());
+            cvInsert.put(VocabularyContract.VocabularyEntry.COLUMN_GROUP_NAME, originalGroup);
+            cvInsert.put(VocabularyContract.VocabularyEntry.COLUMN_TAG, Constants.TAG_LINK);
+            getContentResolver().insert(VocabularyContract.VocabularyEntry.CONTENT_URI,
+                    cvInsert);
+
+            numUpdatedCurrent = 1;
+        }
+        // Current vocabulary does exist in db
+        else {
+            cursor.moveToFirst();
+            long existingId = cursor.getLong(0);
+            String existingGroup = cursor.getString(1);
+
+            if (existingGroup != null || existingGroup != "") {
+                numUpdatedCurrent = getContentResolver().update(VocabularyContract.VocabularyEntry.CONTENT_URI,
+                        cvUpdate,
+                        VocabularyContract.VocabularyEntry.COLUMN_GROUP_NAME + "=?",
+                        new String[]{existingGroup});
+            } else {
+                numUpdatedCurrent = getContentResolver().update(ContentUris.withAppendedId(VocabularyContract.VocabularyEntry.CONTENT_URI, existingId),
+                        cvUpdate,
+                        null,
+                        null);
+            }
+        }
+
+        int updatedTotal = numUpdatedExisting + numUpdatedCurrent;
+
+        Intent intent = new Intent(ACTION_LINK);
+        if (updatedTotal > 0)
+            intent.putExtra(EXTRA_STATUS, Constants.STATUS_SUCCEEDED);
+        else
+            intent.putExtra(EXTRA_STATUS, Constants.STATUS_FAILED);
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+
+        Timber.d(updatedTotal + " vocabularies updated group to " + originalGroup);
+    }
+
+    private void handleActionSearch(final String query) {
+        Timber.d("Stating search");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("vocabulary").document(query);
+
+        final Intent intent = new Intent(ACTION_SEARCH);
+
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    intent.putExtra(EXTRA_STATUS, Constants.STATUS_SUCCEEDED);
+                    if (document.exists()) {
+                        String phonetic = (String) document.get("phonetic");
+                        String translation = (String) document.get("translation");
+                        String definition = (String) document.get("definition");
+                        ClientVocabulary clientVocabulary = new ClientVocabulary();
+                        clientVocabulary.setWord(query);
+                        clientVocabulary.setPhonetic(phonetic);
+                        clientVocabulary.setTranslation(translation);
+                        clientVocabulary.setDefinition(definition);
+                        intent.putExtra(EXTRA_VOCABULARY, clientVocabulary);
+                        Timber.d("DocumentSnapshot data: " + document.getData());
+                    } else {
+                        intent.putExtra(EXTRA_VOCABULARY, Constants.STATUS_FAILED);
+                        Timber.d("No such document");
+                    }
+                } else {
+                    intent.putExtra(EXTRA_STATUS, Constants.STATUS_FAILED);
+                    Timber.d("get failed with " + task.getException());
+                }
+                LocalBroadcastManager.getInstance(VocabularyIntentService.this).sendBroadcast(intent);
+            }
+        });
+    }
+
+
+
+    private void handleActionUpdateStatus(int status, long vocabId) {
+        ContentValues cv = new ContentValues();
+        cv.put(VocabularyContract.VocabularyEntry.COLUMN_STATUS, status);
+        getContentResolver().update(ContentUris.withAppendedId(VocabularyContract.VocabularyEntry.CONTENT_URI, vocabId),
+                cv,
+                null,
+                null);
+    }
+
+    private void handleActionDownload(final String tag) {
+
+        // Create an instance of FirebaseStorage
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        // Create a storage reference from our app
+        StorageReference storageRef = storage.getReference();
+        // Create a reference with an initial file path and name
+        StorageReference pathReference = storageRef.child(tag + "_list");
+
+        Timber.d("----------------------------download started: " + tag + "_list" + "-------------------------------");
+
+        // Download to a local file
+        String filePath = VocabularyIntentService.this.getFilesDir().getPath() + tag + "_list";
+        final File localFile = new File(filePath);
+        localFile.deleteOnExit();
+
+        final Intent notifyIntent = new Intent(ACTION_DOWNLOAD);
+        pathReference.getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Timber.d("--------------------download succeeded-----------------");
+
+                        try {
+                            importToDB(new FileInputStream(localFile), tag);
+
+                            // Send LocalBroadcast to notify the task status
+                            notifyIntent.putExtra(EXTRA_STATUS, Constants.STATUS_SUCCEEDED);
+                            // Broadcasts the Intent to receivers in this app.
+                            LocalBroadcastManager.getInstance(VocabularyIntentService.this).sendBroadcast(notifyIntent);
+
+                            // Only if vocabularies for today is less than the number user set, generate new learning list
+                            int num = VocabularyContentProvider.getVocabularyNumForToday(VocabularyIntentService.this,
+                                    VocabularyContract.VocabularyEntry.STATUS_LEARNING);
+
+                            String dailyCountStr = PreferenceManager.getDefaultSharedPreferences(VocabularyIntentService.this)
+                                    .getString(getString(R.string.pref_daily_count_key), Constants.DEFAULT_DAILY_COUNT);
+                            Timber.d("dailyCount retrieved from sharedPreference is " + dailyCountStr);
+                            int dailyCount = Integer.parseInt(dailyCountStr);
+                            if (num <= dailyCount)
+                                MyJobService.handleActionGenLearnList(dailyCount, getApplicationContext());
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                        // Local temp file has been created
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        Timber.d("--------------------download failed-----------------");
+                        notifyIntent.putExtra(EXTRA_STATUS, Constants.STATUS_FAILED);
+                        // Broadcasts the Intent to receivers in this app.
+                        LocalBroadcastManager.getInstance(VocabularyIntentService.this).sendBroadcast(notifyIntent);
+                        exception.printStackTrace();
+                    }
+                });
+
+    }
+
+    private void handleActionDelete(String deleteTag) {
+        Uri uri = VocabularyContentProvider.buildVocabularyUriWithTag(deleteTag);
+        int numDeleted = getContentResolver().delete(uri, null, null);
+        Timber.d("--------------------deleted " + numDeleted + " entries from " + deleteTag + "-----------------");
+
+        Intent notifyIntent = new Intent(ACTION_DELETE);
+        if (numDeleted > 0)
+            // Send LocalBroadcast to notify the task status
+            notifyIntent.putExtra(EXTRA_STATUS, Constants.STATUS_SUCCEEDED);
+        else
+            notifyIntent.putExtra(EXTRA_STATUS, Constants.STATUS_FAILED);
+
+        // Broadcasts the Intent to receivers in this app.
+        LocalBroadcastManager.getInstance(VocabularyIntentService.this).sendBroadcast(notifyIntent);
+    }
+
+
+
+    private void importToDB(InputStream inputStream, String tag) {
+        List<VocabularyProtos.Vocabulary> vocabularyList = VocabularyReader.parseProto(inputStream);
+        if (vocabularyList != null) {
+            Timber.d("The size of downloaded vocabulary list " + ": " + vocabularyList.size());
+        }
+
+        ContentValues[] cvArr = new ContentValues[vocabularyList.size()];
+        ContentValues cv;
+        VocabularyProtos.Vocabulary vocabulary;
+
+        for (int i = 0; i < vocabularyList.size(); i++) {
+            vocabulary = vocabularyList.get(i);
+            cv = new ContentValues();
+            cv.put(VocabularyContract.VocabularyEntry.COLUMN_WORD, vocabulary.getWord());
+            cv.put(VocabularyContract.VocabularyEntry.COLUMN_PHONETIC, vocabulary.getPhonetic());
+            cv.put(VocabularyContract.VocabularyEntry.COLUMN_DEFINITION, vocabulary.getDefinition());
+            cv.put(VocabularyContract.VocabularyEntry.COLUMN_TRANSLATION, vocabulary.getTranslation());
+            cv.put(VocabularyContract.VocabularyEntry.COLUMN_TAG, tag);
+
+            cvArr[i] = cv;
+        }
+        int numInserted = getContentResolver().bulkInsert(VocabularyContract.VocabularyEntry.CONTENT_URI, cvArr);
+
+        // Delete local file after importing to database
+        if (vocabularyList.size() == numInserted) {
+            String filePath = VocabularyIntentService.this.getFilesDir().getPath() + tag + "_list";
+            File localFile = new File(filePath);
+            localFile.deleteOnExit();
+        }
+
+    }
+}

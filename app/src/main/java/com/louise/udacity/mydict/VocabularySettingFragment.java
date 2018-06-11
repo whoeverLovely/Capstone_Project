8e91f89e51e2ac5a0f33c149288839e439720a5f
yan <leeyan0706@hotmail.com>
2018-06-11 23:15:22 +0800

    Fix preference summary

--- app/src/main/java/com/louise/udacity/mydict/VocabularySettingFragment.java
+++ app/src/main/java/com/louise/udacity/mydict/VocabularySettingFragment.java
@@ -24,13 +24,31 @@ import java.util.Set;
 import timber.log.Timber;
 
 public class VocabularySettingFragment extends PreferenceFragmentCompat
-        implements Preference.OnPreferenceChangeListener{
+        implements Preference.OnPreferenceChangeListener, SharedPreferences.OnSharedPreferenceChangeListener {
 
     Set<String> existingTags;
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.settings);
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        setSummary(sharedPreferences, getString(R.string.pref_list_tag_key));
+        setSummary(sharedPreferences, getString(R.string.pref_daily_count_key));
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(VocabularyIntentService.ACTION_DOWNLOAD);
@@ -71,55 +89,62 @@ public class VocabularySettingFragment extends PreferenceFragmentCompat
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        Timber.d(preference.getKey() + " updated");
 
-        // Set summary for list preference
-        if (preference instanceof ListPreference) {
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue((String) newValue);
-            String entry = (String) listPreference.getEntries()[prefIndex];
-            Timber.d("the daily count entry is " + entry);
-            listPreference.setSummary(entry);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        existingTags = sharedPreferences.getStringSet(getString(R.string.pref_list_tag_key), null);
+
+        preference = (MultiSelectListPreference) preference;
+        Set<String> newTags = ((MultiSelectListPreference) preference).getValues();
+
+        // Download lists which is not in the existingTags
+        for (String tag : newTags) {
+            if (existingTags == null || existingTags.size() == 0 || !existingTags.contains(tag)) {
+                Timber.d("Starting download " + tag);
+                VocabularyIntentService.startActionDownloadVocabulary(getContext(), tag);
+            }
         }
 
-        // Set summary for multiselect list preference
-        if (preference instanceof MultiSelectListPreference) {
-            MultiSelectListPreference multiSelectListPreference = (MultiSelectListPreference) preference;
-            Set<String> tagsValue = (Set<String>) newValue;
-            StringBuilder summary = new StringBuilder();
-            for (String s : tagsValue) {
-                int index = multiSelectListPreference.findIndexOfValue(s);
-                String entry = (String) multiSelectListPreference.getEntries()[index];
-                summary = summary.append(entry).append("; ");
-            }
-            multiSelectListPreference.setSummary(summary);
-
-            // Download and delete related list
-            if (getString(R.string.pref_list_tag_key).equals(preference.getKey())) {
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-                existingTags = sharedPreferences.getStringSet(getString(R.string.pref_list_tag_key), null);
-
-                // Download lists which is not in the existingTags
-                for (String tag : tagsValue) {
-                    int prefIndex = multiSelectListPreference.findIndexOfValue(tag);
-
-                    Timber.d("new tag: " + tag);
-                    if (existingTags == null || existingTags.size() == 0 || !existingTags.contains(tag)) {
-
-                        Timber.d("Starting download " + tag);
-                        VocabularyIntentService.startActionDownloadVocabulary(getContext(), tag);
-                    }
-                }
-
-                // Deleted lists which is in the exsitingTags but not in tags
-                if (existingTags != null) {
-                    for (String tag : existingTags) {
-                        if (tagsValue.size() == 0 || !tagsValue.contains(tag))
-                            VocabularyIntentService.startActionDeleteList(getContext(), tag);
-                    }
-                }
+        // Deleted lists which is in the exsitingTags but not in tags
+        if (existingTags != null) {
+            for (String tag : existingTags) {
+                if (newTags.size() == 0 || !newTags.contains(tag))
+                    VocabularyIntentService.startActionDeleteList(getContext(), tag);
             }
         }
         return true;
+    }
+
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        setSummary(sharedPreferences, key);
+    }
+
+    private void setSummary(SharedPreferences sharedPreferences, String key) {
+
+        // Set summary for daily count
+        if (key.equals(getString(R.string.pref_daily_count_key))) {
+            String dailyCount = sharedPreferences.getString(key, null);
+            if (dailyCount != null) {
+                Preference preferenceDailyCount = findPreference(getString(R.string.pref_daily_count_key));
+                preferenceDailyCount.setSummary(dailyCount);
+            }
+        }
+
+        // Set summary for list tags
+        if (key.equals(getString(R.string.pref_list_tag_key))) {
+            Set<String> listTags = sharedPreferences.getStringSet(getString(R.string.pref_list_tag_key), null);
+
+            if (listTags != null) {
+                Preference preferenceListTags = findPreference(getString(R.string.pref_list_tag_key));
+                StringBuilder tagsString = new StringBuilder();
+                for (String s : listTags) {
+                    MultiSelectListPreference preference = (MultiSelectListPreference)findPreference(key);
+                    int index = preference.findIndexOfValue(s);
+                    tagsString = tagsString.append(preference.getEntries()[index]).append("; ");
+                }
+                preferenceListTags.setSummary(tagsString);
+            }
+        }
     }
 }
